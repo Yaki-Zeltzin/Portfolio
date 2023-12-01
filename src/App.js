04eebe6fa13829ac7dc23b0df60b87b4239416bb
Zeltzin Romero <zeltzin.rom96@gmail.com>
2023-12-01 11:51:06 -0600

    The Header section is created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import './App.css';
 import About from './components/about/About';
 import Nav from './components/nav/Nav';
+import Header from './components/header/Header';
 
 
 function App() {
   return (
     <>
-     <About />
-     <Nav />
+      <Header />
+      <Nav />
+      <About />
     </>
   );
 }
