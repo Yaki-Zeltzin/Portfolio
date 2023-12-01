7bb97f799fe79b210da37e6ddf185d832440c782
Zeltzin Romero <zeltzin.rom96@gmail.com>
2023-12-01 11:45:09 -0600

    The Contact me and Nav section have been created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css';
 import About from './components/about/About';
+import Nav from './components/nav/Nav';
 
 
 function App() {
   return (
     <>
      <About />
+     <Nav />
     </>
   );
 }
